db522eb716d3f6d7fa06987a460b1fe47a0386d7
mcksarge <higgins.fred.s@gmail.com>
2022-03-02 15:31:25 -0800

    Add new comment link

--- my-project/src/components/comments.js
+++ my-project/src/components/comments.js
@@ -3,13 +3,21 @@ import React from "react";
 function Comments({ comments }) {
     const renderComments = comments.map((comment, i) => {
         return (
-            <li key={i}><strong>{comment.name}</strong>- {comment.comment}</li>
+            <li key={i} className="comment"><strong>{comment.name}</strong>: {comment.comment}</li>
         )
     })
 
     return(
         <div>
-            <ul>
+            <div id="new-comment-container">
+                <h3 id="new-comment-title">Enter A New Comment</h3>
+                <form id="new-comment-form">
+                    <input id="comment-input" type="text" placeholder="Enter Comment..."></input>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            <h2 id="comment-title">Comments</h2>
+            <ul id="comment-container">
                 {renderComments}
             </ul>
         </div>
--- my-project/src/components/plantForm.js
+++ my-project/src/components/plantForm.js
@@ -2,14 +2,16 @@ import React from "react";
 
 function PlantForm() {
     return (
-        <div>
-            <h2>Add a new plant</h2>
-            <form>
+        <div id="new-plant-container">
+            <h2 id="new-plant-title">Add a new plant</h2>
+            <form id="new-plant-form">
                 <input type="text" placeholder="plant name"></input>
                 <input type="text" placeholder="water"></input>
                 <input type="text" placeholder="sunlight"></input>
                 <input type="text" placeholder="soil"></input>
                 <input type="text" placeholder="temp"></input>
+                <input type="text" placeholder="image"></input>
+                <button type="submit" id="new-plant-button">Submit</button>
             </form>
         </div>
     )
